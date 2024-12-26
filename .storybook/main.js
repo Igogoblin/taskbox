eb6e5193a9fbd5ecce79b1e0fe8be0dc3080ba00
Ihar Skavysh <igogoblin@gmail.com>
2024-12-26 13:25:52 +0300

    feat: add tasklist component

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,16 +2,26 @@
 const config = {
   // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   stories: ["../src/components/**/*.stories.@(js|jsx)"],
+
   staticDirs: ["../public"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "@chromatic-com/storybook",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
+  docs: {},
+
+  // typescript: {
+  //   reactDocgen: "react-docgen-typescript"
+  // }
 };
 export default config;
